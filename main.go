6051b32b5f2d755283ff451c4b722817a7103df9
Korjavin Ivan <korjavin@gmail.com>
2017-02-21 00:03:48 +0500

    index retrive

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/tidwall/buntdb"
+	"github.com/tidwall/gjson"
 	"log"
 	"strconv"
 )
@@ -25,11 +27,35 @@ func writer(ochan <-chan Order) {
 		})
 	}
 }
-
+func init() {
+	bdb, _ = buntdb.Open("my.db")
+}
 func main() {
+
+	err := bdb.Update(func(tx *buntdb.Tx) error {
+		err := tx.CreateIndex("sid", "*", buntdb.IndexJSON("Status.#[Sid=6].Sid"))
+		return err
+	})
+	checkErr(err)
+	bdb.View(func(tx *buntdb.Tx) error {
+		tx.AscendGreaterOrEqual("sid", `{"Status.Sid":6}`, func(key, value string) bool {
+			var order Order
+			err := json.Unmarshal([]byte(value), &order)
+			if err != nil {
+				fmt.Println("error:", err)
+			}
+			name := gjson.Get(value, `Statuses.#[Sid=6].Sid`)
+
+			fmt.Printf("key=%s, sid=%s\n", key, name)
+			return true
+		})
+		return nil
+	})
+}
+
+func copy() {
 	ochan := make(chan Order)
 	stopchan := make(chan bool)
-	bdb, _ = buntdb.Open("my.db")
 	go getData(ochan, stopchan)
 	go writer(ochan)
 	<-stopchan
